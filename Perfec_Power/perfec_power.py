6dd6ecbef63b0cb85839d2de189ee90382cf3ee5
José Antonio Trejo Gutiérrez <jose.trejog@sat.gob.mx>
2023-06-14 15:50:02 -0600

    An update that significantly improves performance

--- Perfec_Power/perfec_power.py
+++ Perfec_Power/perfec_power.py
@@ -1,14 +1,15 @@
 # In mathematics, a perfect power is a positive integer that can be expressed as an integer power of another positive integer. More formally, n is a perfect power if there exist natural numbers m > 1, and k > 1 such that m^k = n.
 
-from math import sqrt
+from math import log, sqrt
 
 def isPP(n):
-    for k in range(1,int(sqrt(n)+3)):
-        for i in range(0,k):
-            for j in range(0,k):
-                if i**j == n:
-                    return [i,j]
-                    break
+    n = int(n)
+    if n < 4: return None
+    sr = round(sqrt(n),6)
+    if sr == round(sr): return [sr, 2]
+    for m in range(2,int(n/2)):
+        k = round(log(n, m),6)
+        if k == round(k): return [m, k]
     return None
 
 n = input('Enter an integer number: ')
